81f77a3478c2ac107d00d13667d18f782cd10993
Dmitrii <lost.ping@gmail.com>
2026-02-03 14:13:33 +0500

    fix(slack): double unescaping

--- markdown_to_rich.js
+++ markdown_to_rich.js
@@ -3,11 +3,11 @@ const slack = require("./block_kit");
 
 function unescapeHtml(text) {
   return text
-    .replace(/&amp;/g, "&")
     .replace(/&lt;/g, "<")
     .replace(/&gt;/g, ">")
     .replace(/&quot;/g, '"')
-    .replace(/&#39;/g, "'");
+    .replace(/&#39;/g, "'")
+    .replace(/&amp;/g, "&");
 }
 
 function translatePlainText(token) {
